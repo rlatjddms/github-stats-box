dddf386a85814cd010239e5d47d5364adc221f12
rlatjddms <145992500+rlatjddms@users.noreply.github.com>
2026-09-10 21:52:20 +0900

    Update gist content formatting and labels

--- index.js
+++ index.js
@@ -63,11 +63,11 @@ async function updateGist(stats) {
 
     const gistContent =
         [
-            ['⭐', `Total Stars`, humanize(stats.totalStars)],
-            ['➕', countAllCommits ? 'Total Commits' : 'Past Year Commits', humanize(stats.totalCommits)],
+            // ['⭐', `Total Stars`, humanize(stats.totalStars)],
+            ['🔥', countAllCommits ? 'Total Commits' : 'Past Year Commits', humanize(stats.totalCommits)],
             ['🔀', `Total PRs`, humanize(stats.totalPRs)],
-            ['🚩', `Total Issues`, humanize(stats.totalIssues)],
-            ['📦', `Contributed to`, humanize(stats.contributedTo)],
+            ['🎯', `Total Issues`, humanize(stats.totalIssues)],
+            ['👥', `Contributed to`, humanize(stats.contributedTo)],
         ]
             .map((content) => {
                 let line = `${content[1]}:${content[2]}`;
